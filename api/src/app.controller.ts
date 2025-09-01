56fc6c33bc6c5ac65d05a39a2cb22c7937469003
seifalaa111 <168975921+seifalaa111@users.noreply.github.com>
2025-09-01 15:23:48 +0300

    Update app.controller.ts

--- api/src/app.controller.ts
+++ api/src/app.controller.ts
@@ -2,8 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  @Get('status') // <-- decorator only takes the route path
-  getStatus(): { status: string; service: string; timestamp: string } {
+  @Get('status')
+  getStatus() {
     return {
       status: 'ok',
       service: 'api',
